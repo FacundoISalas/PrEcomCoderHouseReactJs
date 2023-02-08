c5d50b101a052af5c386edd8187c8fb34fcb173c
Facundo Salas <fsalas@qplus.com.ar>
2023-02-07 22:43:40 -0300

    imports faltantes roboto

--- ecommercereactjs/src/App.js
+++ ecommercereactjs/src/App.js
@@ -3,6 +3,11 @@ import CssBaseline from '@mui/material/CssBaseline';
 import  NavBar from '../src/components/NavBar';
 import Container from '@mui/material/Container';
 import ItemListContainer  from '../src/components/ItemListContainer';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function App() {
   return (
     <div className="App">
